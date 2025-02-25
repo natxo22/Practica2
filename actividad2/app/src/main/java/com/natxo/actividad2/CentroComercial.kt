a47406dc3e78ae5d4e21ebb12fce5a54471edcd4
natxo22 <153107311+natxo22@users.noreply.github.com>
2025-02-25 15:17:18 +0100

    Add files via upload

---
+++ actividad2/app/src/main/java/com/natxo/actividad2/CentroComercial.kt
@@ -0,0 +1,6 @@
+package com.natxo.actividad2
+
+data class CentroComercial(
+    val name: String
+
+)
---
+++ actividad2/app/src/main/java/com/natxo/actividad2/Tienda.kt
@@ -0,0 +1,16 @@
+package com.natxo.actividad2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Tienda : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tienda)
+
+    }
+}
