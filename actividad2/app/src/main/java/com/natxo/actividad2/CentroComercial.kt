d3b7516e39866dd65ff136d104f44a63be2741ba
natxo22 <153107311+natxo22@users.noreply.github.com>
2025-03-05 19:42:56 +0100

    Delete actividad2 directory

--- actividad2/app/src/main/java/com/natxo/actividad2/CentroComercial.kt
+++
@@ -1,6 +0,0 @@
-package com.natxo.actividad2
-
-data class CentroComercial(
-    val name: String
-
-)
--- actividad2/app/src/main/java/com/natxo/actividad2/dataclass/CentroComercial.kt
+++
@@ -1,8 +0,0 @@
-package com.natxo.actividad2.dataclass
-
-data class CentroComercial(
-    val tienda: String,
-    val direccion: String,
-    val numeroTiendas: Int,
-    val imagenPortada: String,
-)
